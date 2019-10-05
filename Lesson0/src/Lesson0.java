f9f52ab2cb7ead5107f38af1b71e09c4121e117f
Francisco Payés <payesfrancisco@gmail.com>
2019-10-05 11:53:56 +0200

    Fix code format

--- Lesson0/src/Lesson0.java
+++ Lesson0/src/Lesson0.java
@@ -9,43 +9,42 @@ public class Lesson0 {
 	static void helloWorld() {
 		System.out.println("::HELLO WORLD");
 	}
-	
-	
+
 	static List<Integer> scanInts(String message) {
 		System.out.println(message + ": ");
-		
+
 		List<Integer> numbers = new ArrayList<>();
 		Scanner scanner = new Scanner(System.in);
 
 		while (scanner.hasNext()) {
 			try {
 				int integer = scanner.nextInt();
-				
+
 				numbers.add(integer);
 			} catch (InputMismatchException e) {
 				System.out.println("Invalid value!");
 				scanner.next();
 			}
-	        
-	        if (numbers.size() == Lesson0.DEFAULT_INTS_QTY) {
-	        	break;
-	        }
-	    }
-		
+
+			if (numbers.size() == Lesson0.DEFAULT_INTS_QTY) {
+				break;
+			}
+		}
+
 		scanner.close();
-	    
-	    return numbers;
+
+		return numbers;
 	}
-	
+
 	static int[] convertIntListToIntArray(List<Integer> list) {
 		int[] ret = new int[list.size()];
-		
+
 		for (int i = 0; i < ret.length; i++)
 			ret[i] = list.get(i);
-		
+
 		return ret;
 	}
-	
+
 	static int greatest(int[] numbers) {
 		int greatest = Integer.MIN_VALUE;
 
@@ -63,15 +62,15 @@ public class Lesson0 {
 
 		helloWorld();
 		System.out.println("Ex. 1: Find max number");
-		
+
 		List<Integer> integers = Lesson0.scanInts("Enter numbers");
-		
-		int greatestNumber =  greatest(Lesson0.convertIntListToIntArray(integers));
-		
+
+		int greatestNumber = greatest(Lesson0.convertIntListToIntArray(integers));
+
 		System.out.println("MAX: " + greatestNumber);
-		
+
 		System.out.println("::FINISH");
-		
+
 		return;
 	}
 
